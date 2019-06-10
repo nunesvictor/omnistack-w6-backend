77f15542b1d9fe8237ec708692cc298e22cedf0c
Victor Nunes <nunessvictorr@gmail.com>
2019-06-10 16:25:01 -0300

    first commit

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,15 @@
+const Box = require('../models/Box');
+
+class BoxController {
+  async store (req, res) {
+    const box = await Box.create(req.body);
+    return res.json(box);
+  }
+
+  async show (req, res) {
+    const box = await Box.findById(req.params.id).populate('files');
+    return res.json(box);
+  }
+}
+
+module.exports = new BoxController();
